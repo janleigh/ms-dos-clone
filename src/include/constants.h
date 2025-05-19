5004b0f795baa83fc23f8ebe64aa3ef6c9d4c05d
Jan Leigh Muñoz <janleigh@proton.me>
2025-05-19 19:54:12 +0800

    feat: add constants header and implementation for versioning and default content

---
+++ src/include/constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Version information
+extern const char* VERSION;
+extern const char* COPYRIGHT;
+extern const char* LICENSE;
+
+// File system defaults
+extern const char* DEFAULT_README_CONTENT;
+extern const char* DEFAULT_VERSION_CONTENT;
+extern const char* DEFAULT_LICENSE_CONTENT;
+extern const char* DEFAULT_CONFIG_CONTENT;
+
+// Prompt related
+extern const char* PROMPT_PREFIX;
+
+#endif
---
+++ src/utils/constants.c
@@ -0,0 +1,23 @@
+#include "constants.h"
+
+const char* VERSION = "0.1.1";
+const char* COPYRIGHT = "(c) Jan Leigh Munoz and Victor Alexander Ong";
+const char* LICENSE = "Licensed under MIT License";
+
+const char* DEFAULT_README_CONTENT = 
+    "This is a simple MS-DOS Clone.\r\n"
+    "Created by Jan Leigh Munoz and Victor Alexander Ong.\r\n";
+
+const char* DEFAULT_VERSION_CONTENT = 
+    "MS-DOS Clone v0.1.1\r\n";
+
+const char* DEFAULT_LICENSE_CONTENT = 
+    "MIT License\r\n\r\n"
+    "(c) Jan Leigh Munoz and Victor Alexander Ong.\r\n";
+
+const char* DEFAULT_CONFIG_CONTENT = 
+    "# System Configuration\r\n"
+    "FILES=20\r\n"
+    "BUFFERS=10\r\n";
+
+const char* PROMPT_PREFIX = "C:";
